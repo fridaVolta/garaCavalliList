f5fba06f58f5217b73fef02afcdd20a9733bf38b
frida <frida.valecchi@risikamente.it>
2025-10-17 13:55:18 +0200

    primo commit con arraylist

---
+++ src/Cavallo.java
@@ -0,0 +1,40 @@
+public class Cavallo extends Thread{
+
+    private final String name;
+    private int lentezza;
+    private String  primo;
+    public Cavallo(String name, int lentezza) {
+        super();
+        this.name = name;
+        this.lentezza = lentezza;
+    }
+    public Cavallo(String name){
+        super();
+        this.name = name;
+    }
+    /**override del metodo run */
+    @Override
+    public void run(){
+        System.out.println("Cavallo " +
+                name + " comincia il suo galoppo");
+        for (int i = 1; i <= 10; i++) {
+            try {
+                sleep(lentezza);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(name +" cavalca - passo: " + i);
+        }
+        if(Main.getPrimo().equals("")){
+            Main.setPrimo(this.name);
+        }
+    }
+    /**getter per il parametro di lentezza */
+    protected int getLentezza(){
+        return lentezza;
+    }
+    /**setter per il parametro di lentezza */
+    protected void setLentezza(int lentezza){
+        this.lentezza=lentezza;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    static String primo="";
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String tmpS;
+        int tmp;
+        ArrayList<Cavallo> listaCavallo = new ArrayList<Cavallo>();
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("Inserisci il nome del cavallo " + i);
+            tmpS =  input.nextLine();
+            System.out.println("Inserisci la lentezza del cavallo " + i);
+            tmp = input.nextInt();
+            String v = input.nextLine(); //prende il \n
+            Cavallo c=new Cavallo(tmpS, tmp);
+            listaCavallo.add(c);
+        }
+        for(Cavallo c: listaCavallo){
+            c.start();
+        }
+        for(Cavallo c: listaCavallo){
+            try {
+                c.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Il primo cavallo: " + primo);    }
+
+    public static String getPrimo() {
+        return primo;
+    }
+    public static void setPrimo(String primo) {
+        Main.primo = primo;
+    }
+}
